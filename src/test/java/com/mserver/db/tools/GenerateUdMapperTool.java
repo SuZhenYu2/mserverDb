cda3e42e8fa13d0137667c7cd1c78421dad2c9e2
suzy2 <suzy2@lenovo.com>
2017-11-30 21:21:29 +0800

    更新db版本

--- src/test/java/com/mserver/db/tools/GenerateUdMapperTool.java
+++ src/test/java/com/mserver/db/tools/GenerateUdMapperTool.java
@@ -19,7 +19,7 @@ public class GenerateUdMapperTool {
 	public static void main(String[] args) throws Exception {  
 	    // 项目中jar包所在物理路径  
 		//D:/Dev/Repository/com/lenovo/btcp/btcpDbSupport/btcpDbSupport/0.0.1/btcpDbSupport-0.0.1.jar
-	    String jarName = "D:/logs/maven-repo/Repository/com/mserver/db/mserverDbSupport/0.1.0-product/mserverDbSupport-0.1.0-product.jar";  
+	    String jarName = "D:/logs/maven-repo/Repository/com/mserver/db/mserverDbSupport/0.1.5-product/mserverDbSupport-0.1.5-product.jar";  
 	    @SuppressWarnings("resource")
 		JarFile jarFile = new JarFile(jarName);  
 	    Enumeration<JarEntry> entrys = jarFile.entries();  
